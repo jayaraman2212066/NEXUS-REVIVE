d8d9f4a7a0e34ecfa39543154c46a713eb17917d
jayaraman2212066 <jayaraman2212066@ssn.edu.in>
2026-06-21 12:55:11 +0530

    fix: Add component barrel exports and fix PostCSS config

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { DropZone } from './DropZone';
+export { FileCard } from './FileCard';
+export { FormatSelector } from './FormatSelector';
+export { PreviewPanel } from './PreviewPanel';
+export { ReviewSection } from './ReviewSection';
+export { ReviewModal } from './ReviewModal';
+export { LicenseModal } from './LicenseModal';
+export { PaywallModal } from './PaywallModal';
